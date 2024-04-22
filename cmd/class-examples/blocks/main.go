7a34193bff20113ebd8f4020276331aaa220e2e2
Sawa <sawa.gawlau@gmail.com>
2024-04-22 17:14:30 +0200

    set up

--- cmd/class-examples/blocks/main.go
+++ cmd/class-examples/blocks/main.go
@@ -45,9 +45,14 @@ func main() {
     count := Count {Count: 0}
 
     e.GET("/", func(c echo.Context) error {
-        count.Count++
         return c.Render(200, "index", count)
     })
+
+    e.POST("/count", func(c echo.Context) error {
+        count.Count++
+        return c.Render(200, "count", count)
+    })
+
 
     // e.GET("/blocks", func(c echo.Context) error {
     //     startStr := c.QueryParam("start")
