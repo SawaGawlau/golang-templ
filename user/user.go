49bf2e20bd141b232aad8c10def991482f75dff8
Sawa <sawa.gawlau@gmail.com>
2024-04-22 12:56:31 +0200

    update user tests with rw mutex

--- user/user.go
+++ user/user.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 type User struct {
+	mu sync.RWMutex // read-write mutex
 	health int
 }
 
@@ -19,7 +21,9 @@ func NewUser() *User {
 func startUILoop(u *User){
 	ticker := time.NewTicker(time.Second)
 	for {
+		u.mu.RLock()
 		fmt.Printf("user healts: %d\n", u.health)
+		u.mu.RUnlock()
 		<-ticker.C
 	}
 }
@@ -27,11 +31,13 @@ func startUILoop(u *User){
 func startGameLoop(u *User){
 	ticker := time.NewTicker(time.Millisecond * 300)
 	for {
+		u.mu.Lock() // lock read & write
 		u.health -= rand.Intn(10)
 		if u.health <= 0 {
 			fmt.Println("Game Over")
 			break
 		}
+		u.mu.Unlock() // unlock read & write
 		<-ticker.C
 	}
 }
